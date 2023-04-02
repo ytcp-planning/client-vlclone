b154e4776bb456ac2419f8c783b0c03d30a0c01f
Ngoc Yen <ngocyen.bui@estuary.solutions>
2023-04-02 15:35:55 +0700

    img

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     // limit of 25 imageSizes values
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ['./images/logo_vlclone.png', "https://qr.ecaptcha.vn/api/generate/vcb/6691999999/NGUYEN%20NGOC%20HOANG%20LONG?memo=vlclone%209920&is_mask=0&bg=16"]
+    domains: ['./images/logo_vlclone.png']
   }
 }
 
