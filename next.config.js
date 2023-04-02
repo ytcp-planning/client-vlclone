b88dc0809fe0df90aba5e67936ec97756ad57023
Ngoc Yen <ngocyen.bui@estuary.solutions>
2023-04-02 16:03:43 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,14 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:  [],
   images: {
+    loaderFile: './images/logo_vlclone.png',
     disableStaticImages: true,
     // limit of 25 deviceSizes values
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     // limit of 25 imageSizes values
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  images:  ['./images/logo_vlclone.png']
 }
 
 module.exports = nextConfig
